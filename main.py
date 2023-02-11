39209f23c568e9aafe7ea1ab5ada459a7eeec60d
Deniss Dmitrijevs <dod3005@gmail.com>
2023-02-11 23:26:12 +0200

    input fix #2

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def find_mismatch(text):
 
 def main():
     text = input()
-    if (text == 'I'):
+    if 'I' in text:
         text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
